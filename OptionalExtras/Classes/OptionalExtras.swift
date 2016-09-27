861df11409e82251e91fb8f7605b28f079803484
Marcio Klepacz <marcioklepacz@gmail.com>
2016-09-27 13:32:16 -0400

    Add quick help to functions

--- OptionalExtras/Classes/OptionalExtras.swift
+++ OptionalExtras/Classes/OptionalExtras.swift
@@ -10,46 +10,75 @@ import Foundation
 
 public extension Optional {
     
+    /**
+     Unwraps an option, yielding the content of a .some.
+     Or throws `fatalError` with given description
+     
+     */
     func expect(_ desrcription: String) -> Wrapped {
-        guard let val = self else  {  fatalError(desrcription)   }
+        guard let val = self else  { fatalError(desrcription) }
         
         return val;
     }
     
+    /**
+     Returns the contained value or a default.
+     */
     func unwrapOr(_ def: Wrapped) -> Wrapped {
         return self ?? def
     }
     
+    /**
+     Returns the contained value or computes it from a closure.
+     */
     func unwrapOrElse(_ f: () -> Wrapped) -> Wrapped {
         return self.unwrapOr(f())
     }
-    
+
+    /**
+     Returns true if the option is nil
+     */
     func isNone() -> Bool {
         return self == nil;
     }
     
+    /**
+     Returns true if the option is .some
+     */
     func isSome() -> Bool {
         return !isNone();
     }
     
+    /**
+     Applies a function to the contained value (if any), or returns a default (if not).
+     */
     func mapOr<T>(_ def: T, _ f: (Wrapped) -> T) -> T {
         guard let val = self else { return def }
         
         return f(val)
     }
     
+    /**
+     Applies a function to the contained value (if any), or computes a default (if not).
+     */
     func mapOrElse<T>(_ def: () -> T, _ f: (Wrapped) -> T) -> T {
         guard let val = self else  { return def() }
         
         return f(val)
     }
     
+    /**
+     Returns None if the option is nil, otherwise returns optb.
+     */
     func and(_ optb: Optional) -> Optional {
         if self.isSome() && optb.isSome() { return optb }
         
         return nil;
     }
     
+    /**
+     Returns nil if the option is .none, otherwise returns optb.
+     */
     func or(_ optb: Optional) -> Optional {
         if self.isSome() {
             return self;
@@ -62,6 +91,9 @@ public extension Optional {
         return nil;
     }
     
+    /**
+     Returns the option if it contains a value, otherwise calls f and returns the result.
+     */
     func orElse(_ f: () -> Optional) -> Optional {
         let res = f();
         
